6349ae8096a28fd77d8a9a7adaeb79db8f75af83
Константин Ирошников <VTB4050827@region.vtb.ru>
2022-06-23 11:27:15 +0700

    закончил с навигацией

--- Уроки Яна/Navigation/Navigation/ContentView.swift
+++ Уроки Яна/Navigation/Navigation/ContentView.swift
@@ -7,6 +7,29 @@
 
 import SwiftUI
 
+struct ContentViewPush: View {
+    let user: String
+
+    var body: some View {
+        Text("user \(user)")
+    }
+}
+
+struct ContentViewModal: View {
+    @Environment(\.dismiss) var dismiss
+
+    let user: String
+
+    var body: some View {
+        Button("Press to dismiss") {
+            dismiss()
+        }
+        .font(.title)
+        .padding()
+        .background(Color.black)
+    }
+}
+
 struct ContentView: View {
 
     @State private var users = [
@@ -14,10 +37,12 @@ struct ContentView: View {
         "User5", "User6", "User7", "User8", "User9"
     ]
 
+    @State private var isModal = false
+
     var body: some View {
         NavigationView {
                 generateList
-                .navigationBarTitle("Title", displayMode: .inline)
+                .navigationBarTitle("CastomTitle", displayMode: .inline)
                 .navigationBarItems(
                     leading: Button(action: {
                         print("taped sun")
@@ -48,14 +73,32 @@ struct ContentView: View {
     private var generateList: some View {
         List {
             ForEach(users, id: \.self) { user in
-                Text(user)
+                if isHonestIndex(user) {
+                    NavigationLink(destination: ContentViewPush(user: user)) {
+                        Text(user)
+                    }
+                } else {
+                    Button {
+                        self.isModal.toggle()
+                    } label: {
+                        Text(user)
+                    }
+                }
             }.onDelete(perform: { self.onDelete($0) })
+                .sheet(isPresented: $isModal) {
+                    ContentViewModal(user: "00000")
+                }
         }
         .listStyle(InsetListStyle())
     }
 
     private func onDelete(_ index: IndexSet) {
         users.remove(atOffsets: index)
+    }
+
+    private func isHonestIndex(_ value: String) -> Bool {
+        let index = users.firstIndex(where: { $0 == value }) ?? 0
+        return index / 2 == 0
     }
 }
 
