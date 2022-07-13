46bd05470caf2d684a5f4acaebf1145ff58a8f30
Константин Ирошников <VTB4050827@region.vtb.ru>
2022-07-13 23:56:23 +0700

    создал файл проэкта

---
+++ SSS/SSS/SSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSSApp.swift
+//  SSS
+//
+//  Created by Константин Ирошников on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
