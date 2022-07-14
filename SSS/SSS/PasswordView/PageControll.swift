c398ceb7982c71a5c2eafcd8fd6e276350a6c72a
Константин Ирошников <VTB4050827@region.vtb.ru>
2022-07-14 17:22:05 +0700

    закончил вью контроллер ввода пароля

--- SSS/SSS/PasswordView/PageControll.swift
+++ SSS/SSS/PasswordView/PageControll.swift
@@ -9,29 +9,30 @@ import SwiftUI
 
 struct PageControl: View {
 
-//    @Binding private var selectPage: Int
     private var count: Int = 0
+
+    init(count: Int) {
+        self.count = count
+    }
 
     var body: some View {
         HStack(spacing: 10) {
-            ForEach(0..<6) { _ in
-                Circle()
-                    .fill(SSSColor.white)
-                    .stroke(SSSColor.white, style: StrokeStyle(lineWidth: 2, lineCap: .round))
-                    .frame(width: 15, height: 15)
+            ForEach(0..<6) { index in
+                createCircle(full: index < count)
             }
         }
     }
 
+    @ViewBuilder
     private func createCircle(full: Bool) -> some View {
 
         if full {
-            return Circle()
-                .stroke(SSSColor.white, style: StrokeStyle(lineWidth: 2, lineCap: .round))
+            Circle()
+                .fill(SSSColor.white)
                 .frame(width: 15, height: 15)
         } else {
-            return Circle()
-                .fill(SSSColor.white)
+            Circle()
+                .stroke(SSSColor.white, style: StrokeStyle(lineWidth: 2, lineCap: .round))
                 .frame(width: 15, height: 15)
         }
     }
--- SSS/SSS/PasswordView/PasswordView.swift
+++ SSS/SSS/PasswordView/PasswordView.swift
@@ -13,50 +13,52 @@ struct PasswordView: View {
     }
 
     var body: some View {
-        VStack(spacing: Constant.spacing) {
-            VStack(spacing: 0) {
-                Text("Введите пароль")
-                    .foregroundColor(SSSColor.white)
-                    .font(
-                        .system(size: 25)
-                        .weight(.bold)
-                    )
-                PageControl()
-                    .padding()
-                Text("Введите пароль")
-                    .foregroundColor(SSSColor.red)
-                    .font(
-                        .system(size: 10)
-                        .weight(.regular)
-                    )
-                    .padding(0)
-                Spacer()
-                    .frame(minHeight: 15)
-                    .fixedSize()
+        ZStack {
+            SSSColor.black.ignoresSafeArea()
+            VStack(spacing: Constant.spacing) {
+                VStack(spacing: 0) {
+                    Text("Введите пароль")
+                        .foregroundColor(SSSColor.white)
+                        .font(
+                            .system(size: 25)
+                                .weight(.bold)
+                        )
+                    PageControl(count: 2)
+                        .padding()
+                    Text("Введите пароль")
+                        .foregroundColor(SSSColor.red)
+                        .font(
+                            .system(size: 10)
+                                .weight(.regular)
+                        )
+                    Spacer()
+                        .frame(minHeight: 15)
+                        .fixedSize()
+                }
+                HStack(spacing: Constant.spacing) {
+                    createButton(number: 1)
+                    createButton(number: 2)
+                    createButton(number: 3)
+                }
+                HStack(spacing: Constant.spacing) {
+                    createButton(number: 4)
+                    createButton(number: 5)
+                    createButton(number: 6)
+                }
+                HStack(spacing: Constant.spacing) {
+                    createButton(number: 7)
+                    createButton(number: 8)
+                    createButton(number: 9)
+                }
+                HStack(spacing: Constant.spacing) {
+                    Spacer()
+                        .frame(minWidth: 80)
+                        .fixedSize()
+                    createButton(number: 0)
+                    createButtonForDelete()
+                }
             }
-            HStack(spacing: Constant.spacing) {
-                createButton(number: 1)
-                createButton(number: 2)
-                createButton(number: 3)
-            }
-            HStack(spacing: Constant.spacing) {
-                createButton(number: 4)
-                createButton(number: 5)
-                createButton(number: 6)
-            }
-            HStack(spacing: Constant.spacing) {
-                createButton(number: 7)
-                createButton(number: 8)
-                createButton(number: 9)
-            }
-            HStack(spacing: Constant.spacing) {
-                Spacer()
-                    .frame(minWidth: 80)
-                    .fixedSize()
-                createButton(number: 0)
-                createButtonForDelete()
-            }
-        }.background(SSSColor.black)
+        }
     }
 
     private func createButton(number: UInt) -> ButtonCifr {
